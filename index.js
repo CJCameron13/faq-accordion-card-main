2b731efce80c1d25a8404605c7b958f647d48376
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2023-08-09 15:33:11 -0400

    Update index.js
    Deleted unused variables

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const question = document.querySelectorAll(".select-container")
-const arrow = document.querySelectorAll(".arrow")
-const answer = document.querySelectorAll(".answer")
 
 question.forEach((item) => {
     item.addEventListener('click', () => {
